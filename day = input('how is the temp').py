bfae9dc4bd6d181cf863d0033b73328f32ed1c5d
akarsh330 <akarshgowda7711@gmail.com>
2024-12-24 15:41:18 +0530

    Update day = input('how is the temp').py

--- day = input('how is the temp').py
+++ day = input('how is the temp').py
@@ -1,7 +1,4 @@
-name = input('whats your name : ')
-if len(name) < 3 :
-    print('name must be at lest 3 alphabets')
-elif len(name) > 50 :
-    print('name should be under 50 alphabets')
-else :
-    print('the name is fine')
+i = 1
+while i <= 5 :
+print(i)
+print(done)
