80a1d07453f01df36881f63478dcadc54c4f5f6f
akarsh330 <akarshgowda7711@gmail.com>
2024-12-25 15:40:51 +0530

    Update day = input('how is the temp').py

--- day = input('how is the temp').py
+++ day = input('how is the temp').py
@@ -1,4 +1,15 @@
-i = 1
-while i <= 5 :
-print(i)
-print(done)
+guess_count = 0
+guess_limit = 5
+correct_guess = 8
+while guess_count < guess_limit :
+    guess = int(input('guess = '))
+    if guess == correct_guess :
+        print('you made the right guesss')
+        print('congrads')
+        break
+else :
+    print('sorry you failed')
+
+    
+    
+    
